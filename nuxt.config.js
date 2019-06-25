4b76aa31554ef1ed34e6f647f3d35970694c6c82
Alexis M <iamsnkh@pm.me>
2019-06-25 19:05:36 +0200

    :sparkles: generate for gh-pages and deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,11 @@
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/nuxt.snkh/'
+  }
+} : {}
+
 module.exports = {
+  ...routerBase,
   /*
   ** Headers of the page
   */
