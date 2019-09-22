6bc565b81f4bc8acf98397aeb0834aafb599dbbf
Alexis M <iamsnkh@pm.me>
2019-09-22 03:17:13 +0200

    Updates

---
+++ precache-manifest.4d1ff189805dea48312e728dac81610e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4e0eef9f90647c19214b4894a59dea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d420fbf196185aa4655a",
+    "url": "/static/css/main.c78078e2.chunk.css"
+  },
+  {
+    "revision": "0bc5343b615486812b37",
+    "url": "/static/js/2.95235bd1.chunk.js"
+  },
+  {
+    "revision": "d420fbf196185aa4655a",
+    "url": "/static/js/main.7c87441f.chunk.js"
+  },
+  {
+    "revision": "1bcb7109ff3622648116",
+    "url": "/static/js/runtime~main.9e608f84.js"
+  }
+]);
